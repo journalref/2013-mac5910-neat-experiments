4b741e63da10d87670c6bfae40a9475db6e3593d
Albert De La Fuente <vonpupp@gmail.com>
2013-09-27 10:21:11 -0300

    Merged step 3.1 from prior answer

--- 400-start-experiments.sh
+++ 400-start-experiments.sh
@@ -11,5 +11,9 @@
 # 3.
 cd /opt/stack/spe-2013-experiments && python workload-distributor.py full-utilization-02
 
+# 3.1 --- Merged with Anton's answer: https://groups.google.com/forum/#!topic/openstack-neat/yZlsCo9814U
+# The workload-starter.py script should be deployed, configured with the server IP, and automatically started in the VM image on boot. This way you can create a single image, and then create multiple instances which will automatically request the server for the workload. Then, with the workload-distributor.py script you can distribute workload traces to all the VMs at the same time.
+cd /opt/stack/spe-2013-experiments && python workload-starter.py 143.107.45.200 7
+
 # 4.
 
